4b5bd039e0c760816c4b3a565b7d4b1c96b360e8
Predhenapl1834 <predhena@gmail.com>
2019-11-30 23:33:28 +0530

    he...he..

--- src/app/master.service.ts
+++ src/app/master.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MasterService {
+  private baseUrl = 'http://localhost:3963//api';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  public getAssetOrders(): Observable<any>{
+    return this.http.get(this.baseUrl+ '/AssetMaster');
+  }
 }
